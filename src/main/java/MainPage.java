69a4d94741816065043d7a50cc773dc5bf49c8c1
Margarita Demidova <ritler77@yandex.ru>
2024-06-15 01:36:13 +0300

    Спринт 4

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,86 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class MainPage {
+    private final WebDriver webDriver;
+
+    private final By accordionHeaders = By.className("accordion__heading");
+
+    private final By accordionItems = By.xpath(".//div[@class='accordion__panel']/p");
+
+    private final By orderButtonHeader = By.xpath(".//div[starts-with(@class, 'Header_Nav')]//button[starts-with(@class, 'Button_Button')]");
+
+    private final By orderButtonBody = By.xpath(".//div[starts-with(@class, 'Home_RoadMap')]//button[starts-with(@class, 'Button_Button')]");
+
+    private final By cookieAcceptButton = By.id("rcc-confirm-button");
+
+    private final By yandexLogoLink = By.xpath(".//a[starts-with(@class,'Header_LogoYandex')]");
+
+    private final By scooterLogoLink = By.xpath(".//a[starts-with(@class,'Header_LogoScooter')]");
+
+
+    public MainPage(WebDriver driver) {
+        this.webDriver = driver;
+    }
+
+
+    public void waitForLoadItem(int index) {
+        new WebDriverWait(this.webDriver, 3)
+                .until(ExpectedConditions.visibilityOf(this.webDriver.findElements(this.accordionItems).get(index)));
+    }
+
+
+    public void clickOnCookieAcceptButton() {
+        this.webDriver.findElement(this.cookieAcceptButton).click();
+    }
+
+
+    public String getAccordionHeaderText(int index) {
+        return this.webDriver.findElements(this.accordionHeaders).get(index).getText();
+    }
+
+
+    public String getAccordionItemText(int index) {
+        return this.webDriver.findElements(this.accordionItems).get(index).getText();
+    }
+
+
+    public void clickAccordionHeader(int index) {
+        this.webDriver.findElements(this.accordionHeaders).get(index).click();
+    }
+
+
+    public boolean isAccordionItemDisplayed(int index) {
+        return this.webDriver.findElements(this.accordionItems).get(index).isDisplayed();
+    }
+
+
+    public void clickOrderButtonHeader() {
+        this.webDriver.findElement(this.orderButtonHeader).click();
+    }
+
+
+    public void clickOrderButtonBody() {
+        this.webDriver.findElement(this.orderButtonBody).click();
+    }
+
+
+    public String getYandexLogoLink() {
+        return this.webDriver.findElement(this.yandexLogoLink).getAttribute("href");
+    }
+
+
+    public String getScooterLogoLink() {
+        return this.webDriver.findElement(this.scooterLogoLink).getAttribute("href");
+    }
+
+
+    public boolean isYandexLogoLinkOpenedInNewTab() {
+        String blanc = "_blank";
+        String value = this.webDriver.findElement(this.yandexLogoLink).getAttribute("target");
+        return blanc.equals(value);
+    }
+}
---
+++ src/test/java/MainPageLinksTests.java
@@ -0,0 +1,59 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class MainPageLinksTests {
+
+    private WebDriver webDriver;
+
+    private final String mainPageUrl = "https://qa-scooter.praktikum-services.ru";
+
+    private final String yandexUrl = "//yandex.ru";
+
+    private final String scooterUrl = "//qa-scooter.praktikum-services.ru";
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+        this.webDriver = new ChromeDriver();
+        this.webDriver.get(this.mainPageUrl);
+    }
+
+    @After
+    public void tearDown() {
+        this.webDriver.quit();
+    }
+
+
+    @Test
+    public void checkYandexLinkIsCorrect() {
+        MainPage mainPage = new MainPage(this.webDriver);
+
+        assertTrue(
+                "Yandex Logo Link doesn't go to " + this.yandexUrl,
+                mainPage.getYandexLogoLink().contains(this.yandexUrl)
+        );
+
+        assertTrue(
+                "Yandex Logo Link doesn't open in new tab",
+                mainPage.isYandexLogoLinkOpenedInNewTab()
+        );
+    }
+
+
+    @Test
+    public void checkScooterLinkIsCorrect() {
+        MainPage mainPage = new MainPage(this.webDriver);
+
+        assertTrue(
+                "Scooter Logo Link doesn't go to " + this.scooterUrl,
+                mainPage.getScooterLogoLink().contains(this.scooterUrl)
+        );
+    }
+}
